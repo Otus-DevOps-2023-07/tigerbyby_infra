ee9b6891b13a8ae7ef7bf38427cf79797b9216a7
Slava Kshimovsky <tigerbyby@gmail.com>
2023-09-12 22:08:15 +0300

    Main HW

--- terraform/modules/db/main.tf
+++ terraform/modules/db/main.tf
@@ -1,12 +1,12 @@
-######################
-#terraform {
-#  required_providers {
-#    yandex = {
-#      source  = "yandex-cloud/yandex"
-#      version = "~>0.95.0"
-#    }
-#  }
-#}
+#####################
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "~>0.95.0"
+    }
+  }
+}
 
 resource "yandex_compute_instance" "db" {
   name = "reddit-db"
