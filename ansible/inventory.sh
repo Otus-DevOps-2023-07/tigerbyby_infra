64f0ad6375dd1e21f3ae63942c38d76fa0c1ebd6
Slava Kshimovsky <tigerbyby@gmail.com>
2023-09-12 23:34:29 +0300

    Another one fix

--- ansible/inventory.sh
+++ ansible/inventory.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-app_ip=$(yc compute instance get --name reddit-app | grep -A1 one_to_one_nat | awk '/address/ {print $2}')
-db_ip=$(yc compute instance get --name reddit-db | grep -A1 one_to_one_nat | awk '/address/ {print $2}')
+#app_ip=$(yc compute instance get --name reddit-app | grep -A1 one_to_one_nat | awk '/address/ {print $2}')
+#db_ip=$(yc compute instance get --name reddit-db | grep -A1 one_to_one_nat | awk '/address/ {print $2}')
+app_ip=
+db_ip=
 
 if [ "$1" == "--list" ] ; then
 cat<<EOF
