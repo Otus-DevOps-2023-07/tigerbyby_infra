5e2f4547189ea704497072ac51f214f51b90f50f
Slava Kshimovsky <tigerbyby@gmail.com>
2023-09-22 16:13:47 +0300

    Added one star

--- terraform/stage/outputs.tf
+++ terraform/stage/outputs.tf
@@ -4,3 +4,15 @@ output "external_ip_address_app" {
 output "external_ip_address_db" {
   value = module.db.external_ip_address_db
 }
+
+### Ansible inventory file
+resource "local_file" "AnsibleInventory" {
+  content = templatefile("../files/inventory.tmpl",
+   {
+     ip_address_app = module.app.external_ip_address_app
+     db_host = module.db.internal_ip_address_db
+     ip_address_db = module.db.external_ip_address_db
+   }
+ )
+  filename = "../../ansible/environments/stage/inventory"
+}
